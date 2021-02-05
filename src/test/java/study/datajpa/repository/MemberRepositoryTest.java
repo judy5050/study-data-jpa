6920e7d5694ce030c146a7765c62bc5280564ad3
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-02-05 16:55:04 +0900

    스프링 데이터 jpa 페이징과 정렬

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -171,6 +174,36 @@ public class MemberRepositoryTest {
         System.out.println("findMember = "+findMember);
 
 
+
+    }
+
+    @Test
+    public void paging(){
+        //given
+        memberRepository.save(new Member("username1",10));
+        memberRepository.save(new Member("username2",10));
+        memberRepository.save(new Member("username3",10));
+        memberRepository.save(new Member("username4",10));
+        memberRepository.save(new Member("username5",10));
+
+        int age=10;
+        PageRequest pageRequest= PageRequest.of(0,3,Sort.by(Sort.Direction.DESC,"username"));
+
+        //when
+        Page<Member> page=memberRepository.findByAge(age,pageRequest);
+
+        Page<MemberDto> toMap=page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        //then
+
+        List<Member> content=page.getContent();
+        long totalElements = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);//페이지 번호
+        assertThat(page.getTotalPages()).isEqualTo(2);//전체 페이지 번호
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
 
     }
 
